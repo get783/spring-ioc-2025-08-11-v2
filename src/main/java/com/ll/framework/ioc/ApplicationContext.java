e21fd6f6d51c692e3acb0f7ae3cd25a904222500
leejiyeon <sky75013@naver.com>
2025-08-21 10:27:54 +0900

    Reflections 기본 사용

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -3,24 +3,35 @@ package com.ll.framework.ioc;
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
+import com.ll.framework.ioc.annotations.Component;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ApplicationContext {
+    private Reflections reflections;
     private Map<String, Object> beans;
-    private String basePackage;
 
     public ApplicationContext(String basePackage) {
-        this.basePackage = basePackage;
+        reflections = new Reflections(basePackage, Scanners.TypesAnnotated);
         beans = new HashMap<>();
     }
 
     public void init() {
+        Set<Class<?>> components = reflections.getTypesAnnotatedWith(Component.class);
+
+        for (Class<?> component : components) {
+            String className = component.getSimpleName();
+            String classNameToCamelCase = className.substring(0, 1).toLowerCase() + className.substring(1);
+
+            beans.put(classNameToCamelCase, genBean(classNameToCamelCase));
+        }
     }
 
     public <T> T genBean(String beanName) {
-
         Object bean = beans.get(beanName);
         if (bean == null) {
             bean = switch (beanName) {
